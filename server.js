8f57d2c961863d2f7b2b745a137b1397a91f9e98
pkenil01 <patelkenil2312@gmail.com>
2025-11-20 18:37:30 -0500

    Setting up server.js

---
+++ server.js
@@ -0,0 +1,30 @@
+// load the express module
+var express = require("express");
+// create an express application
+var app = express();
+
+// testing 123 (ONLY for testing express server)
+app.use('/',function(req,res){
+res.send('HelloWorld');
+});
+
+// view engine setup
+app.set("view engine", "ejs");
+// allow us to read data from HTML forms
+app.use(express.urlencoded({ extended: true }));
+// use the public folder for static files
+app.use(express.static("public"));
+// load book routes file
+var bookRoutes = require("./routes/books");
+// routes for all paths starting at "/"
+app.use("/", bookRoutes);
+// start the server on port 3000
+app.listen(3000);
+// message to indicate server is running
+console.log("Server started on http://localhost:3000");
+// export the app for testing purposes
+module.exports = app;
+
+
+
+//testing123
